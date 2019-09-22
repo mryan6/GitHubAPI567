817f66dc04a651cb410d5e64f35bd4218cbc3227
Michael S. Ryan <mryan6@stevens.edu>
2019-09-22 11:44:09 -0400

    Initial file commits

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # GitHubAPI567
 HW-04a for SSW-567, practicing use of API and designing code with Tester in mind
+
+[![build status of master](https://travis-ci.org/mryan6/GitHubAPI567.svg?branch=master)](https://travis-ci.org/mryan6/GitHubAPI567)
---
+++ TestGitHub.py
@@ -0,0 +1,12 @@
+"""
+  Michael Ryan
+  SSW-567 HW-04a
+  This file will be used to unit test the implementation of our GitHubAPI requests
+"""
+
+import unittest
+
+class TestGitHub(unittest.TestCase):
+
+    def testSomethin(self) :
+        self.assertEqual(1,1,'1 is equal to 1')
