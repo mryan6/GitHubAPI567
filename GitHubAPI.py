f4662d81902ed52a764c3f88254d687900c4b669
Michael S. Ryan <mryan6@stevens.edu>
2019-09-22 13:32:48 -0400

    initial code and tests

--- GitHubAPI.py
+++ GitHubAPI.py
@@ -1,5 +1,46 @@
 """
   Michael Ryan
   SSW-567 HW-04a
-  This script will use the GitHub API to return the names of a specific user's repos and the number of commits to each one  
+  This script will use the GitHub API to return the names of a specific user's repos and the number of commits to each one
 """
+
+import requests
+import json
+
+def getResponse(username) :
+    url = 'https://api.github.com/users/'+username+'/repos'
+    return requests.get(url)
+
+def getRepos(response) :
+    names = []
+    if response :
+        repos = json.loads(response.text)
+        for repo in repos :
+            names.append(repo['name'])
+
+    return names
+
+def getCommits(username, repos) :
+    if (len(repos) > 0) :
+        for repo in repos :
+            commits = json.loads(requests.get('https://api.github.com/repos/'+username+'/'+repo+'/commits').text)
+            print ('Repo: '+repo+' commits: '+str(len(commits)))
+
+        return True
+    else :
+        return False
+
+
+
+def main():
+    name = input('Please enter your GitHub username: ')
+    response = getResponse(name)
+    if response :
+        repos = getRepos(response)
+        getCommits(name,repos)
+
+    else :
+        Print('Error connecting with API')
+
+if __name__ == "__main__":
+    main()
--- TestGitHub.py
+++ TestGitHub.py
@@ -5,8 +5,34 @@
 """
 
 import unittest
+from GitHubAPI import getRepos, getCommits, getResponse
 
 class TestGitHub(unittest.TestCase):
 
-    def testSomethin(self) :
-        self.assertEqual(1,1,'1 is equal to 1')
+    def testGetResponse(self) :
+        self.assertEqual(getResponse('mryan6').status_code, 200, 'mryan6 is a valid GitHub username, therefore it should return with status code 200')
+
+    def testGetBadResponse(self) :
+        self.assertNotEqual(getResponse('').status_code, 200, 'This is not a valid GitHub username, therefore it should not return with status code 200')
+
+    def testGetRepos(self) :
+        response = getResponse('mryan6')
+        repos = getRepos(response)
+        self.assertEqual(len(repos), 7, 'My own GitHub username has 7 repos associated with it')
+
+    def testGetBadRepos(self) :
+        response = getResponse('')
+        repos = getRepos(response)
+        self.assertEqual(len(repos),0,'This GitHub username does not exist therefore there are no repos to return')
+
+    def testGetCommits(self) :
+        response = getResponse('mryan6')
+        repos = getRepos(response)
+        commits = getCommits('mryan6',repos)
+        self.assertEqual(commits,True,'My own GitHub has repos with commits associated with it')
+
+    def testGetBadCommits(self) :
+        response = getResponse('')
+        repos = getRepos(response)
+        commits = getCommits('',repos)
+        self.assertEqual(commits,False,'This GitHub username does not exist and therefore there are no commits to return')
